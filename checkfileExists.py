30941ba7a38055d6479dfd58932a65684c9aa8a6
Guillaume <43378069+guiguithub@users.noreply.github.com>
2021-02-22 21:21:33 +0100

    Update checkfileExists.py

--- checkfileExists.py
+++ checkfileExists.py
@@ -3,7 +3,7 @@ import os
 #check si fichier existe le crée s'il n'existe pas et retourne le fichier
 def checkfileExists(filename,modeIfExists):
     if os.path.exists(filename):
-        f = file(filename, modeIfExists)
+        f = open(filename, modeIfExists)
     else: #if doesn't exists create file
-        f = file(filename, "w+")
+        f = open(filename, "w+")
     return f
